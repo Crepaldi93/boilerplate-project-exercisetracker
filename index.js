982e2de6e4fb931e1eb759ac3027c3e7738d5c4f
Crepaldi93 <96325460+Crepaldi93@users.noreply.github.com>
2022-12-01 23:03:28 -0300

    feat: connect to mongodb

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+// Connect to mongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Check if mongoose is connected
+console.log(mongoose.connection.readyState);
 
 app.use(cors())
 app.use(express.static('public'))
