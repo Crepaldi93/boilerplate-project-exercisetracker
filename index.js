6009410dc8d95b54974dd8bbd87c508b6753e144
Replit user <>
2022-12-02 13:37:44 +0000

    feat: create mongoose schemas

--- index.js
+++ index.js
@@ -7,6 +7,46 @@ const mongoose = require('mongoose');
 // Connect to mongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// Create user schema
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  exercises: [{
+    type: Schema.Types.ObjectId,
+    ref: "Exercise"
+  }]
+});
+
+mongoose.exports = mongoose.model("User", userSchema);
+
+// Create exercise schema
+const exerciseSchema = new mongoose.Schema({
+  username: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  _id: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  }
+});
+
+mongoose.exports = mongoose.model("Exercise", userSchema);
+
 // Check if mongoose is connected
 console.log(mongoose.connection.readyState);
 
