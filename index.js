de1f36fba4d2d4829cf9a5a7f8b278c0c5577c05
Crepaldi93 <96325460+Crepaldi93@users.noreply.github.com>
2022-12-04 10:47:39 -0300

    feat: add user and get all users

--- index.js
+++ index.js
@@ -7,24 +7,10 @@ const mongoose = require('mongoose');
 // Connect to mongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Create user schema
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true
-  },
-  exercises: [{
-    type: Schema.Types.ObjectId,
-    ref: "Exercise"
-  }]
-});
-
-mongoose.exports = mongoose.model("User", userSchema);
-
 // Create exercise schema
 const exerciseSchema = new mongoose.Schema({
   username: {
-    type: Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: "User"
   },
   description: {
@@ -37,15 +23,28 @@ const exerciseSchema = new mongoose.Schema({
   },
   date: {
     type: Date,
-    required: true,
   },
   _id: {
-    type: Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: "User"
   }
 });
 
-mongoose.exports = mongoose.model("Exercise", userSchema);
+// Create exercise model
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+// Create user schema
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  log: [exerciseSchema]
+});
+
+// Create user model
+const User = mongoose.model("User", userSchema);
+
 
 // Check if mongoose is connected
 console.log(mongoose.connection.readyState);
@@ -56,9 +55,45 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// Use body-parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+// Post new user
+app.post("/api/users", (req, res) => {
+  // Get user input from the html page
+  let htmlUser = req.body.username;
+
+  // Insert htmlUser into mongodb
+  let newUser = new User({username: htmlUser});
+  newUser.save((err, savedUser) => {
+    if (err) return console.error(err);
+    let responseObject = {};
+    responseObject["username"] = savedUser.username;
+    responseObject["_id"] = savedUser.id;
+    res.json(responseObject)
+  });
+});
+
+// Get all users
+app.get("/api/users", (req, res) => {
+  User.find({}, (err, usersArray) => {
+    if (err) return console.error(err);
+    res.json(usersArray);
+  })
+})
+
+// Post new exercise
+app.post("/api/users/:_id/exercises", (req, res) => {
+  
+  
+  res.json({})
+})
 
 
-
+// Export mongoose
+exports.UserModel = User
+exports.ExerciseModel = Exercise
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
